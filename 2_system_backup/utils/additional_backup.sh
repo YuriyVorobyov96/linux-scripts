7edd954b84958df06bfafed6dc3b3b13659fdbfd
YuriyVorobyov96 <darthrevan1333@gmail.com>
2023-08-27 22:50:42 +0300

    Added scripts for system backup

--- 2_system_backup/utils/additional_backup.sh
+++ 2_system_backup/utils/additional_backup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 function additional_backup {
-  tar cpNf /archive "created on `date '+%d-%B-%Y'`.tar" /home /etc/vsftpd.conf /etc/ssh/sshd_config /etc/xrdp/xrdp.ini
+  sudo tar cpNf /archive/"created on `date '+%d-%B-%Y'`.tar" /home /etc/vsftpd.conf /etc/ssh/sshd_config /etc/xrdp/xrdp.ini
 }
