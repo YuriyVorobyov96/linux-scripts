b8efba2340f93764c6bcfecc0702dfd74ea8fd92
YuriyVorobyov96 <darthrevan1333@gmail.com>
2023-08-28 20:36:15 +0300

    Added scripts for system backup

--- 2_system_backup/utils/move_files.sh
+++ 2_system_backup/utils/move_files.sh
@@ -3,5 +3,5 @@
 source /scripts/system_backup/constants/dirs_paths.sh
 
 function move_files_from_tmp_to_archive_dir {
-  mv -f "${TMP_ARCHIVE_PATH}"/* "${ARCHIVE_PATH}"/
+  sudo mv -f "${TMP_ARCHIVE_PATH}"/* "${ARCHIVE_PATH}"/
 }
