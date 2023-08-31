56df254fb4bfa602e25c34b170358d07d17c76f0
YuriyVorobyov96 <darthrevan1333@gmail.com>
2023-08-31 15:48:55 +0300

    Added scripts for system backup

--- 2_system_backup/utils/add_cron.sh
+++ 2_system_backup/utils/add_cron.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 function add_cronjob {
-  cron="30 18 * * 5 /scripts/system_backup/backup.sh"
+  CRON="30 18 * * 5 /scripts/system_backup/backup.sh"
 
-  if ! sudo crontab -l | fgrep -q "$cron" ;
+  if ! sudo crontab -l | fgrep -q "$CRON" ;
     then
       sudo crontab -l > allcrons
-      echo "$cron" >> allcrons
+      echo "$CRON" >> allcrons
       sudo crontab allcrons
       rm allcrons
   fi
